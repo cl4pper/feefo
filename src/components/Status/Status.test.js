b3103986c0c3a10d73ed6463cc58a2a983851900
cl4pper <cl4pper@gmail.com>
2021-08-29 13:48:36 +0100

    test[components]: create tests for Status

---
+++ src/components/Status/Status.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+// COMPONENTS
+import Status from './Status';
+
+let component;
+
+describe('Status component', () => {
+  beforeAll(() => {
+    component = (props) => {
+      return <Status {...props} />;
+    };
+  });
+
+  describe('should render', () => {
+    const props = {
+        status: '8',
+        label: 'Status label',
+    }
+
+    it('status value', () => {
+      const { getByText } = render(component(props));
+  
+      expect(getByText('8%').textContent).toBeTruthy();
+    });
+
+    it('label', () => {
+      const { getByText } = render(component(props));
+  
+      expect(getByText('Status label').textContent).toBeTruthy();
+    });
+  });
+});
